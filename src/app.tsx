283c7a500705004ed00de21a8d595d4c8b216023
Jarvis <1120886013@qq.com>
2021-01-10 18:28:02 +0800

    feat: 添加request.ts与获取用户信息处理

---
+++ src/app.tsx
@@ -0,0 +1,36 @@
+import { history } from 'umi';
+import PageLoading from '@/components/PageLoading';
+import { queryCurrent } from './services/user';
+
+/**
+ * 获取用户信息比较慢的时候会展示一个 loading
+ */
+export const initialStateConfig = {
+  loading: <PageLoading />,
+};
+
+export async function getInitialState(): Promise<{
+  currentUser?: API.CurrentUser;
+  fetchUserInfo?: () => Promise<API.CurrentUser | undefined>;
+}> {
+  const fetchUserInfo = async () => {
+    try {
+      const currentUser = await queryCurrent();
+      return currentUser;
+    } catch (error) {
+      history.push('/user/login');
+    }
+    return undefined;
+  };
+  // 如果是登录页面，不执行
+  if (history.location.pathname !== '/user/login') {
+    const currentUser = await fetchUserInfo();
+    return {
+      fetchUserInfo,
+      currentUser,
+    };
+  }
+  return {
+    fetchUserInfo,
+  };
+}
---
+++ src/services/API.d.ts
@@ -0,0 +1,16 @@
+declare namespace API {
+  export type CurrentUser = {
+    avatar?: string;
+    name?: string;
+    title?: string;
+    group?: string;
+    signature?: string;
+    tags?: {
+      key: string;
+      label: string;
+    }[];
+    userid?: string;
+    access?: 'user' | 'guest' | 'admin';
+    unreadCount?: number;
+  };
+}
