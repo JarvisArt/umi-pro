0ec515d39f52b16501666cccfcf79ab32fa53bc8
Jarvis <1120886013@qq.com>
2021-03-16 23:59:34 +0800

    styles: 调整我的看板样式

--- src/pages/Board/index.tsx
+++ src/pages/Board/index.tsx
@@ -1,4 +1,4 @@
-import { Form, Select, Button } from 'antd';
+import { Select, Button, Tabs } from 'antd';
 import { Chart, LineAdvance } from 'bizcharts';
 import { Responsive, WidthProvider } from 'react-grid-layout';
 import 'react-grid-layout/css/styles.css';
@@ -6,39 +6,40 @@ import 'react-resizable/css/styles.css';
 import styles from './index.less';
 
 const { Option } = Select;
+const { TabPane } = Tabs;
 const ResponsiveGridLayout = WidthProvider(Responsive);
 
 const lineData = [
-  { month: 'Jan', temperature: 7 },
-  { month: 'Jan', temperature: 3.9 },
-  { month: 'Feb', temperature: 13 },
-  { month: 'Feb', temperature: 4.2 },
-  { month: 'Mar', temperature: 16.5 },
-  { month: 'Mar', temperature: 5.7 },
-  { month: 'Apr', temperature: 14.5 },
-  { month: 'Apr', temperature: 8.5 },
-  { month: 'May', temperature: 10 },
-  { month: 'May', temperature: 11.9 },
-  { month: 'Jun', temperature: 7.5 },
-  { month: 'Jun', temperature: 15.2 },
-  { month: 'Jul', temperature: 9.2 },
-  { month: 'Jul', temperature: 17 },
-  { month: 'Aug', temperature: 14.5 },
-  { month: 'Aug', temperature: 16.6 },
-  { month: 'Sep', temperature: 9.3 },
-  { month: 'Sep', temperature: 14.2 },
-  { month: 'Oct', temperature: 8.3 },
-  { month: 'Oct', temperature: 10.3 },
-  { month: 'Nov', temperature: 8.9 },
-  { month: 'Nov', temperature: 5.6 },
-  { month: 'Dec', temperature: 5.6 },
+  { month: 'Jan', city: 'Tokyo', temperature: 7 },
+  { month: 'Jan', city: 'London', temperature: 3.9 },
+  { month: 'Feb', city: 'Tokyo', temperature: 13 },
+  { month: 'Feb', city: 'London', temperature: 4.2 },
+  { month: 'Mar', city: 'Tokyo', temperature: 16.5 },
+  { month: 'Mar', city: 'London', temperature: 5.7 },
+  { month: 'Apr', city: 'Tokyo', temperature: 14.5 },
+  { month: 'Apr', city: 'London', temperature: 8.5 },
+  { month: 'May', city: 'Tokyo', temperature: 10 },
+  { month: 'May', city: 'London', temperature: 11.9 },
+  { month: 'Jun', city: 'Tokyo', temperature: 7.5 },
+  { month: 'Jun', city: 'London', temperature: 15.2 },
+  { month: 'Jul', city: 'Tokyo', temperature: 9.2 },
+  { month: 'Jul', city: 'London', temperature: 17 },
+  { month: 'Aug', city: 'Tokyo', temperature: 14.5 },
+  { month: 'Aug', city: 'London', temperature: 16.6 },
+  { month: 'Sep', city: 'Tokyo', temperature: 9.3 },
+  { month: 'Sep', city: 'London', temperature: 14.2 },
+  { month: 'Oct', city: 'Tokyo', temperature: 8.3 },
+  { month: 'Oct', city: 'London', temperature: 10.3 },
+  { month: 'Nov', city: 'Tokyo', temperature: 8.9 },
+  { month: 'Nov', city: 'London', temperature: 5.6 },
+  { month: 'Dec', city: 'Tokyo', temperature: 5.6 },
   { month: 'Dec', city: 'London', temperature: 9.8 },
 ];
 
 const Board: React.FC = () => {
   const chartLineRender = (
     <div style={{ width: '100%', height: '100%' }}>
-      <div className={`${styles.dragHandle} dragHandle`}>手柄</div>
+      <div className={`${styles.dragHandle} dragHandle`}>{/* 用户增长数 */}</div>
       <div className={styles.chartWrap}>
         <Chart padding={[30, 20, 60, 40]} autoFit data={lineData}>
           <LineAdvance shape="smooth" point area position="month*temperature" color="city" />
@@ -50,19 +51,22 @@ const Board: React.FC = () => {
   return (
     <>
       <div className={styles.header}>
-        <Form layout="inline">
-          <Form.Item label="选择看板">
-            <Select value="默认看板" style={{ width: 150 }}>
-              <Option value="默认看板">默认看板</Option>
-            </Select>
-          </Form.Item>
-          <Form.Item label="查询时间">
-            <Select value="过去七天" style={{ width: 150 }}>
-              <Option value="过去七天">过去七天</Option>
-            </Select>
-          </Form.Item>
-        </Form>
-        <Button type="primary">保存看板</Button>
+        <div className={styles.tabs}>
+          <Tabs defaultActiveKey="1" type="card" size="small">
+            <TabPane tab="用户月增长看板" key="1"></TabPane>
+            <TabPane tab="年度流水看板" key="2"></TabPane>
+            <TabPane tab="总浏览量监控" key="3"></TabPane>
+            <TabPane tab="总浏览量监控" key="4"></TabPane>
+            <TabPane tab="总浏览量监控" key="5"></TabPane>
+          </Tabs>
+        </div>
+        <div>
+          <span className={styles.timeLabel}>查询时间</span>
+          <Select value="过去七天" style={{ width: 110, marginRight: 20 }}>
+            <Option value="过去七天">过去七天</Option>
+          </Select>
+          <Button type="primary">保存看板</Button>
+        </div>
       </div>
       <ResponsiveGridLayout
         className={styles.layout}
